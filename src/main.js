8d6bbdd10c5be228344de51e791d1eb0c1bc97a7
DizzyBHigh <dizzybhigh@gmail.com>
2019-09-02 16:44:18 +0200

    Updated interface and css, need to work on flip

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ $(document).ready(function () {
       'teamdata': [
         {
           name: 'Dizzy High',
-          color: 'warning',
-          items: [
+          captain: true,
+          facts: [
             {'fact': 'I Paid 2 90 year old ladies to go to a nightclub', 'truth': true},
             {'fact': 'I am a secret balloon burster', 'truth': false},
             {'fact': 'I massage the petals of flowers', 'truth': false}
@@ -15,7 +15,7 @@ $(document).ready(function () {
         },
         {
           name: 'Johnny Liarface',
-          color: 'warning',
+          captain: false,
           items: [
             {'fact': 'I masturbate on busses', 'truth': false},
             {'fact': 'green makes me instanly vomit', 'truth': true},
@@ -25,6 +25,7 @@ $(document).ready(function () {
         {
           name: 'Jenny FirePants',
           color: 'warning',
+          captain: false,
           items: [
             {'fact': 'Ive had 9 abortions', 'truth': false},
             {'fact': 'I cooked a meal for the queen', 'truth': false},
@@ -34,12 +35,12 @@ $(document).ready(function () {
       ]
     },
     {
-      'name': 'Team Two',
-      'score': 0,
-      'teamdata': [
+      name: 'Team Two',
+      score: 0,
+      teamdata: [
         {
           name: 'Cece Lederer',
-          color: 'warning',
+          captain: true,
           items: [
             {'fact': 'I often dance naked in my hallway', 'truth': true},
             {'fact': 'Ive shot a lion in Zambia', 'truth': false},
@@ -48,7 +49,7 @@ $(document).ready(function () {
         },
         {
           name: 'Bob the Blaggard',
-          color: 'warning',
+          captain: false,
           items: [
             {'fact': 'Me and Cece made out last night', 'truth': false},
             {'fact': 'I have kept count of how many flies ive killed in my lifetime', 'truth': true},
@@ -60,7 +61,7 @@ $(document).ready(function () {
         },
         {
           name: 'Frankie Fibalot',
-          color: 'warning',
+          captain: false,
           items: [
             {'fact': 'I hold the world record for number of times you can say I in 1 minute', 'truth': false},
             {'fact': 'I can hold my breath for longer than 7 minutes', 'truth': false},
@@ -71,69 +72,16 @@ $(document).ready(function () {
     }
   ];
 
-  let gameRound = [
-    {
-      'round': 'Waiting To Start',
-      'btn': 'Begin',
-      'text': 'Get your Bingo Card',
-      'img': './assets/img/blank-card.jpg'
-    },
-    {
-      'round': 'Round One',
-      'btn': 'Round One',
-      'text': 'One Line or all 4 Corners',
-      'img': './assets/img/one-line.gif'
-    },
-    {
-      'round': 'Round Two',
-      'btn': 'Round Two',
-      'text': 'Two Lines same direction',
-      'img': './assets/img/two-lines.gif'
-    },
-    {
-      'round': 'Round Three',
-      'btn': 'Round Three',
-      'text': 'Three Lines same direction',
-      'img': './assets/img/three-lines.gif'
-    },
-    {
-      'round': 'Final Round',
-      'btn': 'Final Round',
-      'text': 'Full House - all squares',
-      'img': './assets/img/full-house.jpg'
-    }
-  ];
-
-  let gameScreen = '#game-screen';
-  let t1Screen = '#team1-screen';
-  let t2Screen = '#team2-screen';
   // Init The Game
   let initGame = function (gameData) {
     console.log('init');
-    $('#game-screen').show();
-    $('#team1-screen').hide();
-    $('#team2-screen').hide();
-
+   
   };
 
+  let flipCards = function(){
+    $('.gc').toggleClass('gamecard-hover');
+  }
+  $('#reveal').click(flipCards);
   initGame();
 
-
-  var windowWidth = $(window).width();
-  var windowHeight = $(window).height();
-  var slideCount = $('.slide').length;
-
-  $('#game-screen').on('click', function () {
-    
-  });
-
-  $('button#team-one').on('click', function () {
-  
-
-  });
-  
-  $('button#team-two').on('click', function () {
-
-
-})
 });
